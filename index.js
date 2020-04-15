1bc873012bf24e6d9dfc52bf020ae5bc697b2767
Murilo Victor <murilovictor63@gmail.com>
2020-04-15 13:52:04 -0300

    Configurando a view-engine com ejs e adicionando o body-parser

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+const bodyParser = require('body-parser');
 const express = require('express')
 const app = express()
 const port = 3000
 
+
+app.use('view-engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
-    res.send("Bem vindo ao meu Blog....");
+    res.send("Bem vindo ao meu Blog.");
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`))
